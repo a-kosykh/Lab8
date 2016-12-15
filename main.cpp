53074f17abd8e4c857bc43dbd79851489b6f4bb8
Aleksandr Kosykh <kik9399@mail.ru>
2016-12-15 23:15:44 +0300

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,123 @@
+#include <windows.h> 
+#include "stdafx.h"
+LRESULT CALLBACK WinFun(HWND, UINT, WPARAM, LPARAM);
+char WinName[] = "Мое окно"; // Имя класса окна 
+int WINAPI WinMain(HINSTANCE hIns, HINSTANCE hPrevIns, LPSTR arg, int WinMode)
+{
+	HWND hwnd; 
+	MSG msg;  
+	WNDCLASSEX wcl; 
+				
+	wcl.hInstance = hIns;  
+	wcl.lpszClassName = WinName;  
+	wcl.lpfnWndProc = WinFun;
+	wcl.style = 0; 
+	wcl.cbSize = sizeof(WNDCLASSEX); 
+	wcl.hIcon = LoadIcon(NULL, IDI_APPLICATION); 
+	wcl.hIconSm = LoadIcon(NULL, IDI_WINLOGO);
+	wcl.hCursor = LoadCursor(NULL, IDC_ARROW);  
+	wcl.lpszMenuName = NULL; 
+	wcl.cbClsExtra = 0;  
+	wcl.cbWndExtra = 0;
+
+	wcl.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+
+	if (!RegisterClassEx(&wcl)) return 0;
+	
+	hwnd = CreateWindow(
+		WinName, 
+		"Я УМЕЮ РИСОВАТЬ", 
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,  
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT, 
+		CW_USEDEFAULT,  
+		HWND_DESKTOP, 
+		NULL, 
+		hIns, 
+		NULL); 
+			  
+	ShowWindow(hwnd, WinMode);
+	UpdateWindow(hwnd);
+	
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg); 
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+
+
+struct rect
+{
+	int x1, x2, y1, y2;
+};
+
+struct rectList  
+{
+	rect L;
+	rectList *pNext;
+};
+
+rectList *pFirst = 0, *p;
+
+void add(rectList *&pF, rectList *p)
+{  
+	p->pNext = pF;
+	pF = p;
+}
+
+int x1, x2, y1, y2;
+HPEN pB;
+HBRUSH pW;
+
+LRESULT CALLBACK WinFun(HWND hwnd, UINT message,
+	WPARAM wParam, LPARAM lParam)
+{
+	HDC hdc;
+	PAINTSTRUCT ps;
+	switch (message)
+	{
+	case WM_MOUSEMOVE:
+		if (wParam & MK_RBUTTON)  
+		{
+			int x, y;
+			x = LOWORD(lParam);
+			y = HIWORD(lParam);
+			hdc = GetDC(hwnd);
+			HPEN pen = CreatePen(PS_DASHDOTDOT, 1, RGB(139, 0, 255));;
+			HBRUSH br = CreateHatchBrush(HS_DIAGCROSS, RGB(255, 255, 0));
+			SelectObject(hdc, pen);
+			SelectObject(hdc, br);
+			Ellipse(hdc, x - 50, y - 50, x + 50, y + 50);
+			DeleteObject(pen);
+			DeleteObject(br);
+			ReleaseDC(hwnd, hdc);
+		}
+		EndPaint(hwnd, &ps);
+		break;
+	case WM_PAINT:  
+		hdc = BeginPaint(hwnd, &ps);
+		p = pFirst;
+		while (p) 
+		{
+			SelectObject(hdc, pW);
+			SelectObject(hdc, pB);
+			MoveToEx(hdc, p->L.x1, p->L.y1, (LPPOINT)NULL);
+			Rectangle(hdc, p->L.x1, p->L.y1, p->L.x2, p->L.y2);
+			p = p->pNext;
+		}
+		EndPaint(hwnd, &ps);
+		break;
+
+	case WM_DESTROY: 
+		PostQuitMessage(0);
+		break;
+
+	default:
+		
+		return DefWindowProc(hwnd, message, wParam, lParam);
+	}
+	return 0;
+}
